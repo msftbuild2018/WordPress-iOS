5fdfba7fa4a78da6c0ddacbae3e03b29f8a45669
Tony Rankin <tonyr@tonyrankin.com>
2017-06-20 17:47:11 -0700

    Add header view for site settings

---
+++ WordPress/Classes/ViewRelated/Blog/BlogSettingsHeaderView.swift
@@ -0,0 +1,54 @@
+import Foundation
+import WordPressShared
+
+class BlogSettingsHeaderView: UIView {
+    let NO_ICON_STATUS_TEXT = "Add a Site Icon"
+    let UPDATE_ICON_STATUS_TEXT = "Update Site Icon"
+    
+    let siteIconImageView = UIImageView()
+    let statusTextLabel = UILabel()
+    
+    // changing the site icon will automatically update the icon image view
+    var siteIconUrl: String {
+        set {
+            if (newValue != siteIconUrl) {
+                siteIconUrl = newValue
+                updateSiteIcon(url: siteIconUrl)
+            }
+        }
+        get {
+            return siteIconUrl
+        }
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        statusTextLabel.text = NO_ICON_STATUS_TEXT
+        addSubview(statusTextLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+    }
+    
+    private func updateSiteIcon(blog: Blog) {
+        let siteIconPicker = SiteIconPickerPresenter(blog: blog)
+        siteIconPicker.onCompletion = ((media: Media?, error: Error?) -> Void) {
+            if (error) {
+                // show error message
+            } else if (media) {
+            } else {
+            }
+        }
+        // TODO
+        // if null/empty string
+            // set default image
+        // else
+            // start picker presenter
+            // on success
+    }
+}
